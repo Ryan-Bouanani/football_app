52a8d1c43a4759a410c1cbcbbad7145ad4ae4d6a
Omicheri <a.omer69500@yahoo.com>
2024-12-06 14:30:26 +0100

    rajout de recherche via le nom des joueur

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -10,8 +10,7 @@ class TeamDetailsScreen extends StatefulWidget {
 class _TeamDetailsScreenState extends State<TeamDetailsScreen> {
   final ApiService _apiService = ApiService();
   List<dynamic>? _squadDetails;
-  bool _isLoading = true;
-  String? _errorMessage;
+  List<dynamic>? _filteredSquadDetails;
 
   @override
   void initState() {
@@ -20,18 +19,20 @@ class _TeamDetailsScreenState extends State<TeamDetailsScreen> {
   }
 
   Future<void> _fetchTeamData() async {
-    try {
-      final teamDetails = await _apiService.fetchTeamDetails();
-      setState(() {
-        _squadDetails = teamDetails['squad'];
-        _isLoading = false;
-      });
-    } catch (e) {
-      setState(() {
-        _errorMessage = e.toString();
-        _isLoading = false;
-      });
-    }
+    final teamDetails = await _apiService.fetchTeamDetails();
+    setState(() {
+      _squadDetails = teamDetails['squad'];
+      _filteredSquadDetails = _squadDetails;
+    });
+  }
+
+  void _filterPlayers(String query) {
+    setState(() {
+      _filteredSquadDetails = _squadDetails?.where((player) {
+        final playerName = player['name']?.toLowerCase() ?? '';
+        return playerName.contains(query.toLowerCase());
+      }).toList();
+    });
   }
 
   @override
@@ -39,48 +40,50 @@ class _TeamDetailsScreenState extends State<TeamDetailsScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Team Players'),
-        centerTitle: true,
       ),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : _errorMessage != null
-              ? Center(child: Text('Erreur : $_errorMessage'))
-              : ListView.builder(
-                  itemCount: _squadDetails?.length ?? 0,
-                  itemBuilder: (context, index) {
-                    final player = _squadDetails?[index];
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 10, vertical: 5),
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: Card(
-                          elevation: 4,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          child: ListTile(
-                            title: Text(player?['name'] ?? 'Nom inconnu'),
-                            subtitle: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                    'Poste : ${player?['position'] ?? 'Non spécifié'}'),
-                                Text(
-                                    'Nationalité : ${player?['nationality'] ?? 'N/A'}'),
-                                Text(
-                                    'Date de naissance : ${player?['dateOfBirth'] ?? 'N/A'}'),
-                              ],
-                            ),
-                          ),
-                        ).animate().scale(
-                              duration: 500.ms,
-                              curve: Curves.easeInOut,
-                            ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              onChanged: _filterPlayers,
+              decoration: InputDecoration(
+                labelText: 'Search Player',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.search),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _filteredSquadDetails?.length ?? 0,
+              itemBuilder: (context, index) {
+                final player = _filteredSquadDetails?[index];
+                return Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  child: Card(
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: ListTile(
+                      title: Text(player?['name'] ?? 'Nom inconnu'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Poste : ${player?['position'] ?? 'Non spécifié'}'),
+                          Text('Nationalité : ${player?['nationality'] ?? 'N/A'}'),
+                          Text('Date de naissance : ${player?['dateOfBirth'] ?? 'N/A'}'),
+                        ],
                       ),
-                    );
-                  },
-                ),
+                    ),
+                  ).animate().fadeIn(duration: 500.ms).scaleXY(begin: 0.95, end: 1.0),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
