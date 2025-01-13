273d5bc9a4d1b95b4c7367f74bdf19e9929d078e
Ryan <ryan42426@gmail.com>
2025-01-13 11:57:43 +0100

    Added the choice of championship to display

--- lib/src/views/standings_page.dart
+++ lib/src/views/standings_page.dart
@@ -11,19 +11,40 @@ class StandingsPage extends StatefulWidget {
 
 class _StandingsPageState extends State<StandingsPage> {
   List<dynamic> standings = [];
+  List<String> competitions = [];
   final ApiService apiService = ApiService();
   bool isLoading = true;
   String competitionName = '';
+  String selectedCompetition = '';
 
   @override
   void initState() {
     super.initState();
-    fetchStandings();
+    fetchCompetitions();
   }
 
+  Future<void> fetchCompetitions() async {
+    apiService.fetchCompetitions((competitionCodes) {
+      setState(() {
+        competitions = competitionCodes;
+        selectedCompetition = competitions.contains('FL1') ? 'FL1' : (competitions.isNotEmpty ? competitions[0] : '');
+        fetchStandings();
+      });
+    }, () {
+      setState(() {
+        isLoading = false;
+      });
+    });
+  }
+
+
   Future<void> fetchStandings() async {
+    if (selectedCompetition.isEmpty) return;
+    setState(() {
+      isLoading = true;
+    });
     apiService.get(
-      'https://api.football-data.org/v4/competitions/FL1/standings',
+      'https://api.football-data.org/v4/competitions/$selectedCompetition/standings',
       (data) {
         setState(() {
           standings = data['standings'][0]['table'];
@@ -39,49 +60,60 @@ class _StandingsPageState extends State<StandingsPage> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          children: [
-            const Icon(Icons.table_chart, color: Colors.white),
-            const SizedBox(width: 10),
-            Text(
-              competitionName.isNotEmpty ? competitionName : 'Chargement...',
-              style: const TextStyle(color: Colors.white),
-            ),
-          ],
-        ),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.filter_list, color: Colors.white),
-            onPressed: () {
-              // Fonctionnalité de filtrage potentielle
-            },
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Row(
+        children: [
+          const Icon(Icons.table_chart, color: Colors.white),
+          const SizedBox(width: 10),
+          Text(
+            competitionName.isNotEmpty ? competitionName : 'Chargement...',
+            style: const TextStyle(color: Colors.white),
           ),
         ],
       ),
-      body: isLoading 
-        ? Center(
-            child: CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor),
-            )
+      actions: [
+      DropdownButton<String>(
+        value: selectedCompetition.isNotEmpty ? selectedCompetition : null,
+        dropdownColor: Colors.blue,
+        icon: const Icon(Icons.arrow_downward, color: Colors.white),
+        onChanged: (String? newValue) {
+          setState(() {
+            selectedCompetition = newValue!;
+            fetchStandings(); 
+          });
+        },
+        items: competitions.map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value, style: const TextStyle(color: Colors.white)),
+          );
+        }).toList(),
+      ),
+    ],
+    ),
+    body: isLoading 
+      ? Center(
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor),
           )
-        : RefreshIndicator(
-            onRefresh: fetchStandings,
-            child: ListView.builder(
-              itemCount: standings.length,
-              itemBuilder: (context, index) {
-                final team = standings[index];
-                return TeamStandingCard(
-                  team: team, 
-                  index: index, 
-                  standings: standings
-                );
-              },
-            ),
+        )
+      : RefreshIndicator(
+          onRefresh: fetchStandings,
+          child: ListView.builder(
+            itemCount: standings.length,
+            itemBuilder: (context, index) {
+              final team = standings[index];
+              return TeamStandingCard(
+                team: team, 
+                index: index, 
+                standings: standings
+              );
+            },
           ),
-    );
-  }
+        ),
+  );
+}
 }
